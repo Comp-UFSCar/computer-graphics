0ae5b91fd801447df61bb500c03021ad6d1ae14c
Lucas Oliveira David <lucasolivdavid@gmail.com>
2015-09-01 17:56:11 -0300

    Update Polygon.java

--- src/org/CG/drawings/Polygon.java
+++ src/org/CG/drawings/Polygon.java
@@ -10,49 +10,36 @@ import org.CG.infrastructure.Drawing;
  */
 public class Polygon extends Drawing {
 
-    LinkedList<int[]> points;
+    LinkedList<Line> edges;
 
     public Polygon() {
         super();
 
         finished = false;
-        points = new LinkedList<>();
+        edges = new LinkedList<>();
     }
     
     @Override
     public Drawing setStart(int[] start) {
-        points.add(start);
+        edges.add(Line().setStart(start);
         return super.setStart(start);
     }
     
     @Override
     public Drawing translate(int[] point) {
-        point[0] -= start[0];
-        point[1] -= start[1];
-
-        points.stream().forEach((p) -> {
-            p[0] += point[0];
-            p[1] += point[1];
-        });
-
         return this;
     }
     
     @Override
     public void updateLastCoordinateInputted(int[] point) {
-        points.removeLast();
-        points.add(point);
+        edges.getLast().updateLastCoordinateInputted(point);
     }
 
     @Override
     public void draw(GL gl) {
-        gl.glColor3ub(color[0], color[1], color[2]);
-        gl.glBegin(GL.GL_POLYGON);
-
-        points.stream().forEach((point) -> {
-            gl.glVertex2i(point[0], point[1]);
+        edges.stream().forEach((edge) -> {
+            edge.draw(gl);
         });
-
-        gl.glEnd();
     }
 }
+(((
