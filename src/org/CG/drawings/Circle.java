efd06db3ee682c1e16ff6ee63f289bb3242e14eb
Diorge-Mephy <diorge.nla@hotmail.com>
2015-09-03 16:39:56 -0300

    Javadocs and minor refactor for Circle class

--- src/org/CG/drawings/Circle.java
+++ src/org/CG/drawings/Circle.java
@@ -15,20 +15,37 @@ public class Circle extends Drawing {
 
     private int radius;
 
+    /**
+     * Adjusts the center of the circle
+     *
+     * @param point new center of the circle
+     * @return this
+     */
     @Override
     public Drawing translate(Point point) {
         super.translate(point);
         updateLastCoordinate(point);
-
         return this;
     }
 
+    /**
+     * Adjusts the radius of the circle
+     *
+     * @param point one of the points at the edge of the circle
+     * @return this
+     */
     @Override
     public Drawing updateLastCoordinate(Point point) {
         radius = (int) start.euclidianDistance(point);
         return this;
     }
 
+    /**
+     * Draws the circle on screen. Uses a variant of Bresenham's Algorithm with
+     * GL.GL_LINE_LOOP for filling.
+     *
+     * @param gl JOGL object
+     */
     @Override
     public void draw(GL gl) {
         gl.glColor3ub(color.getRed(), color.getGreen(), color.getBlue());
@@ -41,14 +58,7 @@ public class Circle extends Drawing {
         int radiusError = 0;
 
         while (x >= y) {
-            gl.glVertex2i(start.getX() + x, start.getY() + y);
-            gl.glVertex2i(start.getX() - x, start.getY() + y);
-            gl.glVertex2i(start.getX() - x, start.getY() - y);
-            gl.glVertex2i(start.getX() + x, start.getY() - y);
-            gl.glVertex2i(start.getX() + y, start.getY() + x);
-            gl.glVertex2i(start.getX() - y, start.getY() + x);
-            gl.glVertex2i(start.getX() - y, start.getY() - x);
-            gl.glVertex2i(start.getX() + y, start.getY() - x);
+            drawCirclePoints(gl, x, y);
 
             y++;
             radiusError += yChange;
@@ -61,5 +71,18 @@ public class Circle extends Drawing {
         }
 
         gl.glEnd();
+    }
+
+    private void drawCirclePoints(GL gl, int x, int y) {
+        int sx = start.getX();
+        int sy = start.getY();
+        gl.glVertex2i(sx + x, sy + y);
+        gl.glVertex2i(sx - x, sy + y);
+        gl.glVertex2i(sx - x, sy - y);
+        gl.glVertex2i(sx + x, sy - y);
+        gl.glVertex2i(sx + y, sy + x);
+        gl.glVertex2i(sx - y, sy + x);
+        gl.glVertex2i(sx - y, sy - x);
+        gl.glVertex2i(sx + y, sy - x);
     }
 }
