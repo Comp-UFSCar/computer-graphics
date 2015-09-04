0fae27d40960358b2086e605e289e0c4742be601
ldavid <lucasolivdavid@gmail.com>
2015-09-04 08:04:44 -0300

    Invert extension between Circle and Circumference
     Circumference constructor's declaration can now be removed.

--- src/org/CG/drawings/Circle.java
+++ src/org/CG/drawings/Circle.java
@@ -1,8 +1,6 @@
 package org.CG.drawings;
 
-import org.CG.infrastructure.Drawing;
 import javax.media.opengl.GL;
-import org.CG.infrastructure.Point;
 
 /**
  * Circle drawing with Bresenham's Algorithm.
@@ -11,77 +9,9 @@ import org.CG.infrastructure.Point;
  *
  * @author ldavid
  */
-public class Circle extends Drawing {
-
-    private int radius;
+public class Circle extends Circumference {
 
     public Circle() {
         glDrawingType = GL.GL_LINE_LOOP;
-    }
-    
-    /**
-     * Adjusts the center of the circle
-     *
-     * @param point new center of the circle
-     * @return this
-     */
-    @Override
-    public Drawing translate(Point point) {
-        super.translate(point);
-        updateLastCoordinate(point);
-        return this;
-    }
-
-    /**
-     * Adjusts the radius of the circle
-     *
-     * @param point one of the points at the edge of the circle
-     * @return this
-     */
-    @Override
-    public Drawing updateLastCoordinate(Point point) {
-        radius = (int) start.euclidianDistance(point);
-        return this;
-    }
-    
-    /**
-     * Draws the circle on screen. Uses a variant of Bresenham's Algorithm with
-     * GL.GL_LINE_LOOP for filling.
-     *
-     * @param gl JOGL object
-     */
-    @Override
-    protected void drawShape(GL gl) {
-        int x = radius;
-        int y = 0;
-        int xChange = 1 - 2 * radius;
-        int yChange = 1;
-        int radiusError = 0;
-
-        while (x >= y) {
-            drawCirclePoints(gl, x, y);
-
-            y++;
-            radiusError += yChange;
-            yChange += 2;
-            if (2 * radiusError + xChange > 0) {
-                x--;
-                radiusError += xChange;
-                xChange += 2;
-            }
-        }
-    }
-
-    private void drawCirclePoints(GL gl, int x, int y) {
-        int sx = start.getX();
-        int sy = start.getY();
-        gl.glVertex2i(sx + x, sy + y);
-        gl.glVertex2i(sx - x, sy + y);
-        gl.glVertex2i(sx - x, sy - y);
-        gl.glVertex2i(sx + x, sy - y);
-        gl.glVertex2i(sx + y, sy + x);
-        gl.glVertex2i(sx - y, sy + x);
-        gl.glVertex2i(sx - y, sy - x);
-        gl.glVertex2i(sx + y, sy - x);
     }
 }
--- src/org/CG/drawings/Circumference.java
+++ src/org/CG/drawings/Circumference.java
@@ -1,15 +1,81 @@
 package org.CG.drawings;
 
 import javax.media.opengl.GL;
+import org.CG.infrastructure.Drawing;
+import org.CG.infrastructure.Point;
 
 /**
  * Circumference drawing with Bresenham's algorithm
  *
  * @author ldavid
  */
-public class Circumference extends Circle {
+public class Circumference extends Drawing {
 
-    public Circumference() {
-        glDrawingType = GL.GL_POINTS;
+    int radius;
+
+    /**
+     * Adjusts the center of the circle
+     *
+     * @param point new center of the circle
+     * @return this
+     */
+    @Override
+    public Drawing translate(Point point) {
+        super.translate(point);
+        updateLastCoordinate(point);
+        return this;
+    }
+
+    /**
+     * Adjusts the radius of the circle
+     *
+     * @param point one of the points at the edge of the circle
+     * @return this
+     */
+    @Override
+    public Drawing updateLastCoordinate(Point point) {
+        radius = (int) start.euclidianDistance(point);
+        return this;
+    }
+
+    /**
+     * Draws the circle on screen. Uses a variant of Bresenham's Algorithm with
+     * GL.GL_LINE_LOOP for filling.
+     *
+     * @param gl JOGL object
+     */
+    @Override
+    protected void drawShape(GL gl) {
+        int x = radius;
+        int y = 0;
+        int xChange = 1 - 2 * radius;
+        int yChange = 1;
+        int radiusError = 0;
+
+        while (x >= y) {
+            drawCirclePoints(gl, x, y);
+
+            y++;
+            radiusError += yChange;
+            yChange += 2;
+            if (2 * radiusError + xChange > 0) {
+                x--;
+                radiusError += xChange;
+                xChange += 2;
+            }
+        }
+    }
+
+    private void drawCirclePoints(GL gl, int x, int y) {
+        int sx = start.getX();
+        int sy = start.getY();
+        gl.glVertex2i(sx + x, sy + y);
+        gl.glVertex2i(sx - x, sy + y);
+        gl.glVertex2i(sx - x, sy - y);
+        gl.glVertex2i(sx + x, sy - y);
+        gl.glVertex2i(sx + y, sy + x);
+        gl.glVertex2i(sx - y, sy + x);
+        gl.glVertex2i(sx - y, sy - x);
+        gl.glVertex2i(sx + y, sy - x);
     }
 }
