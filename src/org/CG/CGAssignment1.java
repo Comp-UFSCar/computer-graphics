8290c8418c75ab71e930b1a2e88f5992ed18b03d
Diorge-Mephy <diorge.nla@hotmail.com>
2015-09-05 17:15:48 -0300

    Removed unnecessary lines

--- src/org/CG/CGAssignment1.java
+++ src/org/CG/CGAssignment1.java
@@ -29,7 +29,6 @@ import org.CG.infrastructure.Drawing;
  */
 public class CGAssignment1 implements GLEventListener {
 
-    static int[] start, end;
     private static Editor editor;
 
     /**
@@ -154,18 +153,14 @@ public class CGAssignment1 implements GLEventListener {
      */
     @Override
     public void init(GLAutoDrawable drawable) {
-        // Use debug pipeline
-        // drawable.setGL(new DebugGL(drawable.getGL()));
-
         GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
 
         // Enable VSync
         gl.setSwapInterval(1);
 
         // Setup the drawing area and shading mode
         gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        gl.glShadeModel(GL.GL_SMOOTH);
     }
 
     /**
@@ -179,13 +174,11 @@ public class CGAssignment1 implements GLEventListener {
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL gl = drawable.getGL();
-        GLU glu = new GLU();
 
         if (height <= 0) {
             height = 1;
         }
 
-        final float h = (float) width / (float) height;
         gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL.GL_PROJECTION);
         gl.glLoadIdentity();
@@ -201,7 +194,6 @@ public class CGAssignment1 implements GLEventListener {
     @Override
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
-        GLU glu = new GLU();
 
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
@@ -210,7 +202,6 @@ public class CGAssignment1 implements GLEventListener {
             d.draw(gl);
         });
 
-        // Flush all drawing operations to the graphics card
         gl.glFlush();
     }
 
