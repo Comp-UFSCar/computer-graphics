14a83cceab6339a3263a05d73a7c76d6983838fe
ldavid <lucasolivdavid@gmail.com>
2015-09-07 11:21:41 -0300

    Remove unnecessary imports

--- src/org/CG/infrastructure/DrawingsLoader.java
+++ src/org/CG/infrastructure/DrawingsLoader.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.JarURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLDecoder;
