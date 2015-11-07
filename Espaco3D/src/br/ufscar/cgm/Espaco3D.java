7117801a6a397b520786961ba8ffc381a6c54fce
Camilo Moreira <camilomoreira91@gmail.com>
2015-11-07 17:11:06 -0200

    Adicionado novas classes para elementos 3D.

---
+++ Espaco3D/src/br/ufscar/cgm/Espaco3D.java
@@ -0,0 +1,117 @@
+package br.ufscar.cgm;
+
+import br.ufscar.cgm.utils.Drawer;
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
+import com.sun.opengl.util.GLUT;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+
+/**
+ * Espaco3D.java <BR>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel) <P>
+ *
+ * This version is equal to Brian Paul's version 1.2 1999/10/21
+ */
+public class Espaco3D implements GLEventListener {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Simple JOGL Application");
+        GLCanvas canvas = new GLCanvas();
+
+        canvas.addGLEventListener(new Espaco3D());
+        frame.add(canvas);
+        frame.setSize(640, 480);
+        final Animator animator = new Animator(canvas);
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // Run this on another thread than the AWT event queue to
+                // make sure the call to Animator.stop() completes before
+                // exiting
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        animator.stop();
+                        System.exit(0);
+                    }
+                }).start();
+            }
+        });
+        // Center frame
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        animator.start();
+    }
+
+    public void init(GLAutoDrawable drawable) {
+        // Use debug pipeline
+        // drawable.setGL(new DebugGL(drawable.getGL()));
+
+        GL gl = drawable.getGL();
+        System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+        // Enable VSync
+        gl.setSwapInterval(1);
+
+        // Setup the drawing area and shading mode
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        if (height <= 0) { // avoid a divide by zero error!
+        
+            height = 1;
+        }
+        
+        
+        final float h = (float) width / (float) height;
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45.0f, h, 2.0, 20.0);
+        glu.gluLookAt(5.0, 6.0, 7.0,
+                0.0, 0.0, 0.0,
+                0.0, 1.0, 0.0);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    public void display(GLAutoDrawable drawable) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+        GLUT glut = new GLUT();
+
+        // Clear the drawing area
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        // Reset the current matrix to the "identity"
+        gl.glLoadIdentity();
+        
+        //Drawer.drawLine2D(gl, -1, -1, 0, 0);
+        Drawer.drawCube(gl, 1, 0, 0, 0);
+        Drawer.drawCube(gl, 2, 0, 0, 0);
+        Drawer.drawCube(gl, 3, 0, 0, 0);
+        
+        gl.glColor3f(1.0f, 0f, 0f);
+        //glut.glutWireCube(1.0f);
+
+        // Flush all drawing operations to the graphics card
+        gl.glFlush();
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+}
+
---
+++ Espaco3D/src/br/ufscar/cgm/utils/Drawer.java
@@ -0,0 +1,279 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufscar.cgm.utils;
+
+import com.sun.opengl.util.BufferUtil;
+import java.nio.IntBuffer;
+import javax.media.opengl.GL;
+
+public class Drawer {
+    
+    private static int precision = 1000;
+    
+    public static void drawPixel2D(GL gl, double x, double y) {
+        drawPixel3D(gl,x,y,0);
+    }
+    
+    public static void drawPixel3D(GL gl, double x, double y, double z) {
+        gl.glBegin(GL.GL_POINTS);
+        gl.glVertex3d(x, y, z);
+        gl.glEnd();
+    }
+    
+    public static void drawLine2D(GL gl, int x0, int y0, int x1, int y1) {
+
+        /* Não é mais necessário essa conversão.
+        IntBuffer buffer = BufferUtil.newIntBuffer(4);
+        gl.glGetIntegerv(GL.GL_VIEWPORT, buffer);
+        int height = buffer.get(3);
+
+        y0 = height - y0;
+        y1 = height - y1;*/
+        
+        x0 = x0*precision;
+        y0 = y0*precision;
+        x1 = x1*precision;
+        y1 = y1*precision;
+
+        int dx = 0, dy = 0, incE, incNE, d;
+        double x, y;
+        // Condição para saber se a inclinação da reta
+        // está entre 0 e 45 ou entre 46 e 90 graus.
+        boolean dx_menor_dy = false;
+        // Usado para saber se a reta é 
+        // crescente ou decrescente
+        int dy_signal = 1;
+
+        dx = x1 - x0;
+        dy = y1 - y0;
+
+        if (Math.abs(dx) < Math.abs(dy)) {
+            dx_menor_dy = true;
+
+            // Troca dx e dy
+            int aux = dx;
+            dx = dy;
+            dy = aux;
+
+            // Troca x0 e y0
+            aux = x0;
+            x0 = y0;
+            y0 = aux;
+
+            // Troca x1 e y1
+            aux = x1;
+            x1 = y1;
+            y1 = aux;
+        }
+        
+        if (x1 < x0) {
+            int aux = x1;
+            x1 = x0;
+            x0 = aux;
+
+            aux = y1;
+            y1 = y0;
+            y0 = aux;
+
+            dx = -dx;
+            dy = -dy;
+        }
+
+        // Se dy é negativo, a reta é decrescente.
+        // Mudamos o valor de dy para ser desenhado entre 0 e 45 graus
+        // Mas ao invés de incrementar, y é decrementado (por dy_signal)
+        if (dy < 0) {
+            dy_signal = -1;
+            dy = -dy;
+        }
+
+        //Cálculo das variáveis do algoritmo
+        d = 2 * dy - dx;
+        incE = 2 * dy;
+        incNE = 2 * (dy - dx);
+
+        x = x0;
+        y = y0;
+
+        while (x <= x1) {
+
+            if (!dx_menor_dy) {
+                drawPixel2D(gl, x/precision, y/precision);
+            } else {
+                drawPixel2D(gl, y/precision, x/precision);
+            }
+
+            x++;
+            if (d <= 0) {
+                d = d + incE;
+            } else {
+                d = d + incNE;
+                y += dy_signal;
+            }
+
+        }
+
+    }
+    
+    public static void drawLine3D(GL gl, int x0, int y0, int z0, int x1, int y1, int z1) {
+        
+        x0 = x0*precision;
+        y0 = y0*precision;
+        z0 = z0*precision;
+        x1 = x1*precision;
+        y1 = y1*precision;
+        z1 = z1*precision;
+        
+
+        int dx_1 = 0, dx_2 = 0, dy = 0, dz = 0; 
+        int incE_y, incNE_y, incE_z, incNE_z; 
+        int d_1, d_2;
+        double x, y, z;
+        // Condição para saber se a inclinação da reta
+        // está entre 0 e 45 ou entre 46 e 90 graus.
+        boolean dx_menor_dy = false;
+        boolean dx_menor_dz = false;
+        // Usado para saber se a reta é 
+        // crescente ou decrescente
+        int dy_signal = 1;
+        int dz_signal = 1;
+
+        dx_1 = dx_2 = x1 - x0;
+        dy = y1 - y0;
+        dz = z1 - z0;
+
+        if (Math.abs(dx_1) < Math.abs(dy)) {
+            dx_menor_dy = true;
+
+            // Troca dx e dy
+            int aux = dx_1;
+            dx_1 = dy;
+            dy = aux;
+
+            // Troca x0 e y0
+            aux = x0;
+            x0 = y0;
+            y0 = aux;
+
+            // Troca x1 e y1
+            aux = x1;
+            x1 = y1;
+            y1 = aux;
+        }
+        
+        if (Math.abs(dx_2) < Math.abs(dz)) {
+            dx_menor_dz = true;
+
+            // Troca dx e dy
+            int aux = dx_2;
+            dx_2 = dz;
+            dz = aux;
+
+            // Troca x0 e y0
+            aux = x0;
+            x0 = z0;
+            z0 = aux;
+
+            // Troca x1 e y1
+            aux = x1;
+            x1 = z1;
+            z1 = aux;
+        }
+        
+        if (x1 < x0) {
+            int aux = x1;
+            x1 = x0;
+            x0 = aux;
+
+            aux = y1;
+            y1 = y0;
+            y0 = aux;
+            
+            aux = z1;
+            z1 = z0;
+            z0 = aux;
+
+            dx_1 = -dx_1;
+            dx_2 = -dx_2;
+            dy = -dy;
+            dz = -dz;
+        }
+
+        // Se dy é negativo, a reta é decrescente.
+        // Mudamos o valor de dy para ser desenhado entre 0 e 45 graus
+        // Mas ao invés de incrementar, y é decrementado (por dy_signal)
+        if (dy < 0) {
+            dy_signal = -1;
+            dy = -dy;
+        }
+
+        if (dz < 0) {
+            dz_signal = -1;
+            dz = -dz;
+        }
+
+        //Cálculo das variáveis do algoritmo
+        d_1 = 2 * dy - dx_1;
+        d_2 = 2 * dz - dx_2;
+        incE_y = 2 * dy;
+        incNE_y = 2 * (dy - dx_1);
+        incE_z = 2 * dz;
+        incNE_z = 2 * (dz - dx_2);
+
+        x = x0;
+        y = y0;
+        z = z0;
+
+        while (x <= x1) {
+
+            if (!dx_menor_dy && !dx_menor_dz) {
+                drawPixel3D(gl, x/precision, y/precision, z/precision);
+            } else if (!dx_menor_dz) {
+                drawPixel3D(gl, y/precision, x/precision, z/precision);
+            } else if (!dx_menor_dy) {
+                drawPixel3D(gl, z/precision, y/precision, x/precision);
+            } else {
+                drawPixel3D(gl, y/precision, x/precision, x/precision);
+                //drawPixel3D(gl, z/precision, x/precision, x/precision);
+            }
+
+            x++;
+            
+            if (d_1 <= 0) {
+                d_1 = d_1 + incE_y;
+            } else {
+                d_1 = d_1 + incNE_y;
+                y += dy_signal;
+            }
+            
+            if (d_2 <= 0) {
+                d_2 = d_2 + incE_z;
+            } else {
+                d_2 = d_2 + incNE_z;
+                z += dz_signal;
+            }
+
+        }
+
+    }
+    
+    public static void drawCube(GL gl, int r, int x, int y, int z){
+        Drawer.drawLine3D(gl, x+r, y+r, z+r, x-r, y+r, z+r);
+        Drawer.drawLine3D(gl, x+r, y+r, z+r, x+r, y-r, z+r);
+        Drawer.drawLine3D(gl, x+r, y+r, z+r, x+r, y+r, z-r);
+        Drawer.drawLine3D(gl, x+r, y+r, z-r, x-r, y+r, z-r);
+        Drawer.drawLine3D(gl, x+r, y+r, z-r, x+r, y-r, z-r);
+        Drawer.drawLine3D(gl, x+r, y-r, z+r, x-r, y-r, z+r);
+        Drawer.drawLine3D(gl, x+r, y-r, z+r, x+r, y-r, z-r);
+        Drawer.drawLine3D(gl, x-r, y+r, z+r, x-r, y-r, z+r);
+        Drawer.drawLine3D(gl, x-r, y+r, z+r, x-r, y+r, z-r);
+        Drawer.drawLine3D(gl, x-r, y-r, z+r, x-r, y-r, z-r);
+        Drawer.drawLine3D(gl, x-r, y+r, z-r, x-r, y-r, z-r);
+        Drawer.drawLine3D(gl, x+r, y-r, z-r, x-r, y-r, z-r);
+    }
+
+
+}
