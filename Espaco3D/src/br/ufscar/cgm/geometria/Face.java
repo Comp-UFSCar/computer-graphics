94d2f38831317d8b25dcae4f472ac34fe8042edd
br_ss <brufsc@hotmail.com>
2015-11-08 12:58:22 +0000

    Nova Face

--- Espaco3D/src/br/ufscar/cgm/geometria/Face.java
+++ Espaco3D/src/br/ufscar/cgm/geometria/Face.java
@@ -10,8 +10,32 @@ import java.util.ArrayList;
 public class Face {
     
     public ArrayList<Aresta3D> arestas = new ArrayList<Aresta3D>();
+    public Pontos3D ptParaCalcNorma;
+    public Ponto3D Vnormal;
+    public double Intensidade;
     
-    public Face(ArrayList<Aresta3D> as){
+    public Face(ArrayList<Aresta3D> as, Pontos3D pt){
         arestas = as;
+        ptParaCalcNorma = pt;
+        Vnormal = normalDaFace(pt);
+        
+    }
+    
+    public Ponto3D normalDaFace(Pontos3D arg)
+    {
+        Ponto3D v = arg.v();
+        Ponto3D w = arg.w();
+        return new Ponto3D(((v.x * w.z)-(w.y*v.z)), ((-(v.x*w.z))-(w.x*v.z)), ((v.x*w.y)-(w.x*v.y)));
+    }
+    
+    public void ReflexaoDifusa(int Ip, int Kd, Ponto3D direcao)
+    {
+        int produtoEscalar = Vnormal.x * direcao.x + Vnormal.y * direcao.y + Vnormal.z * direcao.z;
+	Intensidade =  Ip * Kd * produtoEscalar;
+    }
+    
+    @Override
+    public String toString(){
+        return ("Normal :" + Vnormal.toString());
     }
 }
