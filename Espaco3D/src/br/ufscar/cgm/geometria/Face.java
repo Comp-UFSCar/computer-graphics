0abe3e29665e5fe68b5a0ad762301dd2dbe8af26
br_ss <brufsc@hotmail.com>
2015-11-11 00:41:10 +0000

    Comentários classe Face

--- Espaco3D/src/br/ufscar/cgm/geometria/Face.java
+++ Espaco3D/src/br/ufscar/cgm/geometria/Face.java
@@ -7,6 +7,16 @@ package br.ufscar.cgm.geometria;
 
 import java.util.ArrayList;
 
+/**
+ * Classe que representa a face do polígono. A face possui como atributos uma lista de arestas (usadas para desenhar 
+ * o "contorno" de cada face), um atributo do tipo Pontos3D para armaznar 3 pontos dessa face (3 pontos não colineares
+ * determinam um plano, e a partir da dos coeficientes da equação do plano será calculado o vetor normal) e um atributo
+ * do tipo Ponto3D para representar o vetor normal. Na construção de uma instância da classe Face o vetor normal dessa 
+ * face é calculado para ser utilizado nas equações de determinação de intensidade. <br>
+ * @author João Paulo RA:408034
+ * @author Breno Silveira RA:551481
+ * @author Camilo Moreira RA:359645
+ */
 public class Face {
     
     public ArrayList<Aresta3D> arestas = new ArrayList<Aresta3D>();
@@ -17,9 +27,17 @@ public class Face {
         arestas = as;
         ptParaCalcNorma = pt;
         vetorNormal = normalDaFace(pt); 
-        //System.out.println(this.toString());
+        
     }
 
+    /**
+     * O cálculo da normal da face é feito determinando 2 vetores que pertencem ao plano a partir dos pontos informados.
+     * Assim, a partir cálculo do determinante são da matriz formada por esses vetores, obtém-se os valores
+     * de i, j, k do vetor normal.
+     * 
+     * @param arg: representa o conjunto de pontos 3D para calcular o vetor normal
+     * @return o vetor normal representado por uma instância da classe Ponto3D
+     */
     private Ponto3D normalDaFace(Pontos3D arg)
     {
         Ponto3D v = arg.v();
@@ -29,6 +47,18 @@ public class Face {
         return normal;
     }
     
+    /**
+     * Para determinar a intendidade, primeiramente é realizado o cálculo do produtoEscalar entre o vetor normal
+     * da face e a direção da luz. Após isso é feito o cálculo da norma da direção e do vetor normal. Por fim, a fórmula
+     * para se obter a intensidade da luz é calculada (considerando a luz ambiente e a luz difusa).
+     * 
+     * @param Ia - intensidade da luz ambiente
+     * @param Ka - constante de reflexão da luz ambiente
+     * @param Ip - intensidade da luz pontual
+     * @param Kd - constante de reflexão da luz difusa
+     * @param direcao - vetor que representa a direcao da luz
+     * @return intensidade da luz (considerando a luz ambiente e a luz difusa)
+     */
     public float getIntensidade(float Ia, float Ka, float Ip, float Kd, Ponto3D direcao)
     {
         double produtoEscalar = vetorNormal.x * direcao.x + vetorNormal.y * direcao.y + vetorNormal.z * direcao.z;
@@ -41,6 +71,14 @@ public class Face {
 	return (float) (Ia*Ka + (Ip * Kd * produtoEscalar) / (normaDoNormal*normaDaDirecao));
     }
     
+    /**
+     * Método auxiliar para determinar a maior distância de uma face e um ponto (no caso, o ponto que representa
+     * a deireção da câmera). A partir dos pontos que formam a face é determinadp a maior distância por meio
+     * do cálculo da distância de cada ponto do vetor passado como parâmetro, por meio do método distanciaAte da
+     * classe Ponto3D.
+     * @param camera - vetor com a direção da câmera
+     * @return a distância máxima da face até a câmera
+     */
     public double getMaiorDistanciaAtePonto(Ponto3D camera){
         ArrayList<Ponto3D> pontos = new ArrayList<Ponto3D>(4);
         for(Aresta3D a : arestas){
